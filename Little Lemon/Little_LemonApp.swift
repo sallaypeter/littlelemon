2346884fedbeca77e654a460f5b0c9ad5e0ae88f
Peter Sallay <psallay@t-email.hu>
2024-02-02 14:59:14 +0100

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Péter Sallay on 2024.02.02.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
